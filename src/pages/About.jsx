8c6f1d1faff25e1180137060c5749ddd0cdb4e59
Peoples Champion <95397105+ThePeoplesChamp@users.noreply.github.com>
2022-07-09 13:43:43 +0930

    Friend Finder App

---
+++ src/pages/About.jsx
@@ -0,0 +1,19 @@
+function About() {
+  return (
+    <>
+      <h1 className='text-6xl mb-4'>Gitfriend Finder</h1>
+      <p className='mb-4 text-2xl font-light'>
+        Enter the name of the friend you're looking for and watch them flock!
+      </p>
+
+      <p className='text-lg text-gray-400'>
+        Layout By:
+        <a className='text-muted' href='https://twitter.com/hassibmoddasser'>
+          Jacob Clement
+        </a>
+      </p>
+    </>
+  )
+}
+
+export default About
